d94c0ea3b854a2787c34780db51f91c3b22e1003
Hemal <h.n.varambhia@gmail.com>
2015-06-24 11:55:12 +0100

    Testing out the mongrel2 ubuntu package

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,31 +9,7 @@
 include_recipe "chef-zeromq"
 include_recipe "chef-ruby"
 
-package "libsqlite3-dev" do
- action :install
-end
-
-package "git" do
- action :install
-end
-
-remote_file "mongrel2-v1.9.1.tar.gz" do
- source "https://github.com/zedshaw/mongrel2/releases/download/v1.9.1/mongrel2-v1.9.1.tar.gz"
- action :create
-end
-
-execute "unpack-mongrel2" do
- command "tar -xzf mongrel2-v1.9.1.tar.gz -C /usr/local/src/"
- action :run
-end
-
-execute "build-mongrel2" do
- command "make clean all && sudo make install"
- cwd "mongrel2-v1.9.1"
- action :run
-end
-
-package "uuid-runtime" do
+package "mongrel2-run" do
  action :install
 end
 
